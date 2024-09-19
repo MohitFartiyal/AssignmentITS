63d9e06a875db035a803d8e83107cc217ea2db3a
MohitFartiyal <fartiyalmohit88@gmail.com>
2024-09-19 10:31:05 +0530

    Add files via upload

---
+++ agge.py
@@ -0,0 +1,6 @@
+birthYear=2002 #Given
+currentYear=int(input("Enter your Currnet YEar"))
+class AgeCalculator:
+    ageInYear=currentYear-birthYear
+age= AgeCalculator()
+print(age.ageInYear)
---
+++ converter.py
@@ -0,0 +1,4 @@
+print ("Convert Rupees into Dollars")
+rupeesAmount = int(input("Enter the amount in rupees"))
+rupeesIntoDolar =(rupeesAmount/84)
+print(rupeesAmount,"Convert into Dollar",rupeesIntoDolar)
